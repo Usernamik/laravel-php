4a3699613677150ad0288b06b697382830906129
Usernamik <danylo.lisovskyi@oa.edu.ua>
2026-06-01 10:29:03 +0000

    Add PostController and BlogPostRepository

--- app/Http/Controllers/Api/Blog/Admin/BaseController.php
+++ app/Http/Controllers/Api/Blog/Admin/BaseController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Blog\Admin;
+namespace App\Http\Controllers\Api\Blog\Admin;
 
-use App\Http\Controllers\Blog\BaseController as GuestBaseController;
+use App\Http\Controllers\Api\Blog\BaseController as GuestBaseController;
 
 abstract class BaseController extends GuestBaseController
 {
--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,34 @@
-<?php
+    <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Blog\PostController;
-use App\Http\Controllers\Api\Blog\Admin\CategoryController;
-use App\Models\BlogCategory;
-use App\Models\BlogPost;
-use App\Models\User;
-use App\Models\Team;
-
-// Пости
-Route::group(['namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
-    Route::apiResource('posts', PostController::class)->names('blog.posts');
-});
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\Api\Blog\PostController;
+    use App\Http\Controllers\Api\Blog\Admin\PostController as AdminPostController;
+    use App\Http\Controllers\Api\Blog\Admin\CategoryController;
+    use App\Models\BlogCategory;
+    use App\Models\BlogPost;
+    use App\Models\User;
+    use App\Models\Team;
 
 
-$groupData = [
-    'namespace' => 'App\Http\Controllers\Api\Blog\Admin',
-    'prefix' => 'admin/blog',
-];
-Route::group($groupData, function () {
-    //BlogCategory
-    $methods = ['index','store','update',];
-    Route::apiResource('categories', CategoryController::class)
-        ->only($methods)
-        ->names('blog.admin.categories');
-});
+    // Пости
+    Route::group(['namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
+        Route::apiResource('posts', PostController::class)->names('blog.posts');
+    });
+
+
+    $groupData = [
+        'namespace' => 'App\Http\Controllers\Api\Blog\Admin',
+        'prefix' => 'admin/blog',
+    ];
+    Route::group($groupData, function () {
+        //BlogCategory
+        $methods = ['index','store','update',];
+        Route::apiResource('categories', CategoryController::class)
+            ->only($methods)
+            ->names('blog.admin.categories');
+
+        Route::apiResource('posts', AdminPostController::class)
+            ->except(['show'])
+            ->names('blog.admin.posts');
+    });
