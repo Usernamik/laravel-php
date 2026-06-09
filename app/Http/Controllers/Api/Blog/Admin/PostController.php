2ac92a1a5d41fda259f0d61df306ee6d457b180d
Usernamik <danylo.lisovskyi@oa.edu.ua>
2026-06-09 15:22:00 +0000

    Lab 9: Blog posts CRUD with validation and repositories

--- app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\Api\Blog\Admin;
 
 use App\Repositories\BlogPostRepository;
+use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogPostUpdateRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 
 class PostController extends BaseController
 {
-    public function __construct(private BlogPostRepository $blogPostRepository)
-    {
+    public function __construct(
+        private BlogPostRepository $blogPostRepository,
+        private BlogCategoryRepository $blogCategoryRepository
+    ) {
         //parent::__construct();
     }
     public function index()
@@ -37,10 +43,34 @@ class PostController extends BaseController
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+
+        public function update(BlogPostUpdateRequest $request, string $id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) {
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->all();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+        if (empty($item->published_at) && $data['is_published']) {
+            $data['published_at'] = Carbon::now();
+        }
+        $result = $item->update($data);
+
+        if ($result) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено'
+            ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
     }
+
 
     /**
      * Remove the specified resource from storage.
--- app/Repositories/BlogPostRepository.php
+++ app/Repositories/BlogPostRepository.php
@@ -17,7 +17,14 @@ class BlogPostRepository extends CoreRepository
         $result = $this->startConditions()
             ->select($columns)
             ->orderBy('id', 'DESC')
+            ->with([
+                'category' => function ($query) {
+                    $query->select(['id', 'title']);
+                },
+                'user:id,name',
+            ])
             ->paginate(25);
+
 
         return $result;
     }
